816cec3693016d6e535feead2ca47312ec631203
Vadim <tvainform@yandex.ru>
2024-11-11 21:11:03 +0500

    module_3_5 исправил ошибки

--- module_2_4.py
+++ module_2_4.py
@@ -3,19 +3,20 @@ primes = []
 not_primes = []
 
 for i in numbers:
-    if i < 2:
+    if i == 1:
         continue
 
     is_prime = True
 
     for j in range(2, i):
-        is_prime = (i % j != 0)
+        is_prime = i % j != 0
         if not is_prime:
-            not_primes.append(i)
             break
 
     if is_prime:
         primes.append(i)
+    else:
+        not_primes.append(i)
 
 print(primes)
 print(not_primes)
