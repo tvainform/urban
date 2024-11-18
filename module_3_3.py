f462abf9cf5c094742deb5ab463f2298088785cf
Vadim <tvainform@yandex.ru>
2024-11-18 21:33:43 +0500

    Добавил вызов функции с различными аргументами

--- module_3_3.py
+++ module_3_3.py
@@ -2,6 +2,10 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 
+print_params()                                # Вызов функции без аргументов
+print_params(5, 'function', False)   # Вызов функции с тремя аргументами
+print_params(466, 'urban')              # Вызов функции с двумя аргументами + 1 по умолчанию
+print_params(a = 1985)                        # Вызов функции с одним аргументом + 2 по умолчанию
 
 print_params(b = 25)
 print_params(c = [1,2,3])
