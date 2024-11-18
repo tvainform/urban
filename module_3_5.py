f11d70cdecba6919b1a8b209d1d68674840c4a26
Vadim <tvainform@yandex.ru>
2024-11-18 21:09:44 +0500

    Добавил проверку нуля

--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,8 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    while str_number.endswith('0'):
+        str_number = str_number[:len(str_number) - 1]
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
