6e2f9340fc4d31b4fe7000dd6fced357058f8a7b
Vadim <tvainform@yandex.ru>
2024-12-09 19:07:19 +0500

    добавил проверку на тип класса Plant, добавил проверку с подменой типа

--- module_6_1.py
+++ module_6_1.py
@@ -5,12 +5,15 @@ class Animal:
         self.name = name
 
     def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
+        if isinstance(food, Plant):
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
         else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+            print(f'Ошибочка: "{food}" вообще - не растение')
 
 class Plant:
     edible = False
@@ -45,3 +48,5 @@ a1.eat(p1)
 a2.eat(p2)
 print(a1.alive)
 print(a2.fed)
+
+print(a1.eat('цветок'))
