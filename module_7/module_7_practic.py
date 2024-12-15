919a8a7d93050ebab393f5a1dd329990c4d8a7c9
Vadim <tvainform@yandex.ru>
2024-12-15 14:07:04 +0500

    module_7_practic

---
+++ module_7/module_7_practic.py
@@ -0,0 +1,12 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('Проводник')
+window.geometry('350x350')
+window.configure(bg='black')
+window.resizable(False, False)
+text = tkinter.Label(window, text='Файл', height=5, width=20, background='silver')
+text.grid(column=1, row=1)
+button_select = tkinter.Button(window, width=20, height=5, text='Выбрать файл')
+button_select.grid(column=1, row=2)
+window.mainloop()
