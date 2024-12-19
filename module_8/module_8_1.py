411be82b0fff552053a13c0566f664bcb9078ef2
Vadim <tvainform@yandex.ru>
2024-12-19 22:16:21 +0500

    module_8_1

---
+++ module_8/module_8_1.py
@@ -0,0 +1,14 @@
+# Задание "Программистам всё можно":
+
+def add_everything_up(a, b):
+    try:
+        res = a + b
+    except TypeError:
+        return f'{a}{b}'
+    else:
+        return round(res, 3)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
