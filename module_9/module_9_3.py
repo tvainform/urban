7798ad7e44f5074e16990c7a47580e80e09ce1cd
Vadim <tvainform@yandex.ru>
2024-12-29 13:07:21 +0500

    module_9_3

---
+++ module_9/module_9_3.py
@@ -0,0 +1,8 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+second_result = (len(first[k]) == len(second[k]) for k in range(0, len(first)))
+
+print(list(first_result))
+print(list(second_result))
