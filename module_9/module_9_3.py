f3ec7288bbde07a9a7d81ffd4325d445e0cbc986
Vadim <tvainform@yandex.ru>
2025-01-13 20:25:09 +0500

    Для первой сборки применил модуль для разности длин

--- module_9/module_9_3.py
+++ module_9/module_9_3.py
@@ -1,7 +1,7 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+first_result = (abs(len(x) - len(y)) for x, y in zip(first, second) if len(x) != len(y))
 second_result = (len(first[k]) == len(second[k]) for k in range(0, len(first)))
 
 print(list(first_result))
